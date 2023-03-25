58ef5b9ba3dce82bb823b034f247f648e8be8c6f
Cenora6 <aleksandra.gasidlo@gmail.com>
2023-03-25 15:29:05 +0100

    Update post

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -3,12 +3,12 @@ import { getPost } from "~/models/post.server";
 import { useLoaderData } from "@remix-run/react";
 
 export default function PostRoute() {
-  const { post } = useLoaderData()
+  const { title, body } = useLoaderData()
 
   return (
     <main className="mx-auto max-w-4x1">
-      <h1 className="my-6 border-b-2 text-center text-3x1">{post.title}</h1>
-      <p>{post.body}</p>
+      <h1 className="my-6 border-b-2 text-center text-3x1">{title}</h1>
+      <p>{body}</p>
     </main>
   );
 }
@@ -16,5 +16,5 @@ export default function PostRoute() {
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params
   const post = await getPost(slug)
-  return json({ post });
+  return json({ title: post.title, body: post.body });
 }
