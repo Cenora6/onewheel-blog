b904a752cab87295a81b7ba4108e2f3858a752d1
Cenora6 <aleksandra.gasidlo@gmail.com>
2023-03-25 15:04:12 +0100

    Move posts to models

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
+import { getPosts } from "~/models/post.server";
 
 interface Post {
   id: number;
@@ -27,27 +28,7 @@ export default function PostsRoute() {
   )
 }
 
-const posts = [
-  {
-    id: 1,
-    slug: "my-first-post",
-    title: "My first post",
-    body: "This is my first post",
-  },
-  {
-    id: 2,
-    slug: "my-second-post",
-    title: "My second post",
-    body: "This is my second post",
-  },
-  {
-    id: 3,
-    slug: "my-third-post",
-    title: "My third post",
-    body: "This is my third post",
-  }
-]
-
 export const loader = async() => {
+  const posts = await getPosts()
   return json({posts})
 }
