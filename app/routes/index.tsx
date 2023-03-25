064c0b08cdd4301f397091b521bd49f48285a821
Cenora6 <aleksandra.gasidlo@gmail.com>
2023-03-25 14:50:17 +0100

    Add posts route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -66,11 +66,15 @@ export default function Index() {
           </div>
         </div>
 
+        <div className="mx-auto mt-16 max-w-7xl text-center">
+          <Link to='/posts' className="text-xl text-blue-600 underline">Blog Posts</Link>
+        </div>
+
         <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
               {
-                src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
+                src: "https://user-images.githubuserco  ntent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
                 alt: "Fly.io",
                 href: "https://fly.io",
               },
